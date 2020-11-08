372aa03f6bd936a7e71399fa6e80c0bb860710dc
Ilya Shinkarenko <shinych@gmail.com>
2020-11-08 21:38:58 +0100

    Fix parameters for Raspberry PI 4

--- tema/test.sh
+++ tema/test.sh
@@ -2,4 +2,10 @@
 
 cd "$(dirname "$0")"
 
-./tema -s 0 -f fonts/6x10.bdf --led-rows=16 --led-row-addr-type=2 --led-multiplexing=3 $@
+# Extra options:
+#  1) fix for Raspberry Pi 4 (s. issue 861)
+#    --led-slowdown-gpio=2
+#  2) options for LysonLed SMD3535 16*32
+#    --led-rows=16 --led-row-addr-type=2 --led-multiplexing=3
+
+./tema -s 0 -f fonts/6x10.bdf --led-slowdown-gpio=2 --led-rows=16 --led-row-addr-type=2 --led-multiplexing=3 $@
