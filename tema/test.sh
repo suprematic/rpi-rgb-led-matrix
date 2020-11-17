bf5c0b4c69baa9835eba7b908aac45480fb110b2
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2020-11-17 14:05:12 +0100

    Specify LED matrix P10

--- tema/test.sh
+++ tema/test.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 # Extra options:
 #  1) fix for Raspberry Pi 4 (s. issue 861)
 #    --led-slowdown-gpio=2
-#  2) options for LysonLed SMD3535 16*32
+#  2) options for LysonLed SMD3535 16*32 P10-3535-4S-sHL 1.0
 #    --led-rows=16 --led-row-addr-type=2 --led-multiplexing=3
 
 ./tema -s 0 -f fonts/6x10.bdf --led-slowdown-gpio=2 --led-rows=16 --led-row-addr-type=2 --led-multiplexing=3 $@
