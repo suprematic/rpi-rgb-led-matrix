6d98cb2f057b9695fc835f27d42ddb8b595497d5
Ilya Shinkarenko <shinych@gmail.com>
2020-12-04 17:12:05 +0100

    fix simple text

--- tema/py/simpletext.py
+++ tema/py/simpletext.py
@@ -7,19 +7,20 @@ import time
 
 class SimpleText(SampleBase):
     def __init__(self, *args, **kwargs):
-        super(RunText, self).__init__(*args, **kwargs)
+        super(SimpleText, self).__init__(*args, **kwargs)
         self.parser.add_argument("-t", "--text", help="The text to display on the RGB LED panel", default="Hello!")
         
-    def display(msg):
+    def run(self):
+        msg = self.args.text
+        print("Displaying '{0}'".format(msg))
         canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("../fonts/6x10.bdf")
         textColor = graphics.Color(255, 255, 0)
         canvas.Clear()
         graphics.DrawText(canvas, font, 0, 10, textColor, msg)
-
-    def run(self):
-        display(self.args.text)        
+        canvas = self.matrix.SwapOnVSync(canvas)
+        time.sleep(1)
 
 # Main function
 if __name__ == "__main__":
