09cb3f66f0b9c73ac837047d46c38807d3777b4f
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2022-12-29 19:42:02 +0100

    Fix int to string convertion

--- bindings/python/7c/m1.py
+++ bindings/python/7c/m1.py
@@ -148,17 +148,17 @@ class SevenCourtsM1(SampleBase):
         t1_service = "." if t1_on_serve else ""
         t2_service = "." if t2_on_serve else ""
         
-        graphics.DrawText(self.canvas, FONT_SCORE, x_set1, y_T1, COLOR_SCORE_SET, t1_set1)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_set2, y_T1, COLOR_SCORE_SET, t1_set2)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_set3, y_T1, COLOR_SCORE_SET, t1_set3)
+        graphics.DrawText(self.canvas, FONT_SCORE, x_set1, y_T1, COLOR_SCORE_SET, str(t1_set1))
+        graphics.DrawText(self.canvas, FONT_SCORE, x_set2, y_T1, COLOR_SCORE_SET, str(t1_set2))
+        graphics.DrawText(self.canvas, FONT_SCORE, x_set3, y_T1, COLOR_SCORE_SET, str(t1_set3))
         graphics.DrawText(self.canvas, FONT_SCORE, x_service, y_T1-y_service_delta, COLOR_SCORE_SERVICE, t1_service)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_game, y_T1, COLOR_SCORE_GAME, t1_game)
+        graphics.DrawText(self.canvas, FONT_SCORE, x_game, y_T1, COLOR_SCORE_GAME, str(t1_game))
 
-        graphics.DrawText(self.canvas, FONT_SCORE, x_set1, y_T2, COLOR_SCORE_SET, t2_set1)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_set2, y_T2, COLOR_SCORE_SET, t2_set2)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_set3, y_T2, COLOR_SCORE_SET, t2_set3)
+        graphics.DrawText(self.canvas, FONT_SCORE, x_set1, y_T2, COLOR_SCORE_SET, str(t2_set1))
+        graphics.DrawText(self.canvas, FONT_SCORE, x_set2, y_T2, COLOR_SCORE_SET, str(t2_set2))
+        graphics.DrawText(self.canvas, FONT_SCORE, x_set3, y_T2, COLOR_SCORE_SET, str(t2_set3))
         graphics.DrawText(self.canvas, FONT_SCORE, x_service, y_T2-y_service_delta, COLOR_SCORE_SERVICE, t2_service)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_game, y_T2, COLOR_SCORE_GAME, t2_game)
+        graphics.DrawText(self.canvas, FONT_SCORE, x_game, y_T2, COLOR_SCORE_GAME, str(t2_game))
 
     def display_match(self, match):
 
