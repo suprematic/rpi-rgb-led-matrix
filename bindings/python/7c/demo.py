771bca70c70447a97ffdb31aa72b09ab14355ef6
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2023-01-03 17:43:58 +0100

    tweak wip

--- bindings/python/7c/demo.py
+++ bindings/python/7c/demo.py
@@ -373,7 +373,7 @@ class M1_Demo(SampleBase):
 
         self.show_title_text(canvas, "Check the CAMPO 1\nto see in action", duration)
 
-        self.show_title_text(canvas, "Special PadelTrend price:\n\nXS1 399€    M1 999€\n\nAny other size: on request", duration)
+        self.show_title_text(canvas, "SPECIAL PadelTrend PRICE\n\nXS1 399€    M1 999€\n\nAny other size: on request", duration)
 
         
 
@@ -381,8 +381,8 @@ class M1_Demo(SampleBase):
     def run_slide_show(self, duration, title_duration):
         canvas = self.matrix.CreateFrameCanvas()
 
-        self.show_title_text(canvas, "Special PadelTrend price:\n\nXS1 399€    M1 999€\n\nAny other size: on request", duration)
-
+        self.show_score_doubles_with_flags_long(canvas, True, duration)
+        
         self.run_demo_sequence(canvas, duration, title_duration)        
 
         #self.show_flags(canvas, duration)
