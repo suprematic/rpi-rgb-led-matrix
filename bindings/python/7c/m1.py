26590c647f22b954e0221245fec293140367b0ea
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2023-01-11 14:24:31 +0100

    tweak

--- bindings/python/7c/m1.py
+++ bindings/python/7c/m1.py
@@ -159,7 +159,7 @@ class SevenCourtsM1(SampleBase):
                 graphics.DrawText(self.canvas, font, x, y, color, str(score))
             else:
                 score = str(int(score) % 10)
-                fill_rect(self.canvas, x-2, y, width_in_pixels(font, score)+1, -y_font_offset(font)-2, color)
+                fill_rect(self.canvas, x-2, y, width_in_pixels(font, score)+2, -y_font_offset(font)-2, color)
                 graphics.DrawText(self.canvas, font, x, y, COLOR_BLACK, score)
 
     def display_score(self, match):
