27fb8f69b3026b2f7a616f2fb677a23b74b5fee6
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2023-01-11 14:23:55 +0100

    tweak >10

--- bindings/python/7c/m1.py
+++ bindings/python/7c/m1.py
@@ -159,8 +159,7 @@ class SevenCourtsM1(SampleBase):
                 graphics.DrawText(self.canvas, font, x, y, color, str(score))
             else:
                 score = str(int(score) % 10)
-                # FIXME looks stupid with "1" in default font => center "1"?
-                fill_rect(self.canvas, x-1, y, width_in_pixels(font, score)+2, -y_font_offset(font)-2, color)
+                fill_rect(self.canvas, x-2, y, width_in_pixels(font, score)+1, -y_font_offset(font)-2, color)
                 graphics.DrawText(self.canvas, font, x, y, COLOR_BLACK, score)
 
     def display_score(self, match):
