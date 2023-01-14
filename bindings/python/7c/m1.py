1b1027bf8526537438242dc11e30bc76677931d4
Ilya Shinkarenko <shinych@gmail.com>
2023-01-14 17:42:20 +0100

    dark

--- bindings/python/7c/m1.py
+++ bindings/python/7c/m1.py
@@ -149,8 +149,8 @@ class SevenCourtsM1(SampleBase):
 
     def display_clock(self):
 
-        image = Image.open("images/logos/mup_52x64.png").convert('RGB')
-        self.canvas.SetImage(image, 12, 0)
+        image = Image.open("images/logos/mup_dark_45x56.png").convert('RGB')
+        self.canvas.SetImage(image, 12,4)
 
         #draw_text(self.canvas, 4, 60, "Court 6", FONT_M, COLOR_GREY_DARK)
 
