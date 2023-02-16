155ab0196ee19b8890656e8f5ab61ed7184d8672
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2023-02-16 20:58:25 +0100

    fuerte-55x64-full.png

--- bindings/python/7c/m1.py
+++ bindings/python/7c/m1.py
@@ -149,8 +149,8 @@ class SevenCourtsM1(SampleBase):
 
     def display_clock(self):
 
-        image = Image.open("images/logos/fuerte-60x64.png").convert('RGB')
-        self.canvas.SetImage(image, 16, 0)
+        image = Image.open("images/logos/fuerte-55x64-full.png").convert('RGB')
+        self.canvas.SetImage(image, 20, 0)
 
         #draw_text(self.canvas, 4, 60, "Court 6", FONT_M, COLOR_GREY_DARK)
 
