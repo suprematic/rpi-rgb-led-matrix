bfb28019c4a6a1caf7ab4e9c0dfd419e66ce1896
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2023-02-16 20:51:57 +0100

    use fuerte-60x64.png

--- bindings/python/7c/m1.py
+++ bindings/python/7c/m1.py
@@ -149,7 +149,7 @@ class SevenCourtsM1(SampleBase):
 
     def display_clock(self):
 
-        image = Image.open("images/logos/gaeta-60x64.png").convert('RGB')
+        image = Image.open("images/logos/fuerte-60x64.png").convert('RGB')
         self.canvas.SetImage(image, 16, 0)
 
         #draw_text(self.canvas, 4, 60, "Court 6", FONT_M, COLOR_GREY_DARK)
